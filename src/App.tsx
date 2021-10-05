8fcb7230e8a38f5d7f7c983ba000e852b97d0804
Felipe Escarassatti <felipeescarassatti@gmail.com>
2021-10-05 18:13:33 -0300

    aplicação de lista de tarefas pronta

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from './App.styles';
+import { useState} from 'react';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
 
-function App() {
+
+const App = () => {
+  const [ list, setList ] = useState<Item[]>([
+    {id: 0, name: 'Comprar o pão da padaria', done: false},
+    {id: 1, name: 'Comprar um bolo na padaria', done: false},
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    })
+    setList(newList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+        <C.Area>
+          <C.Header>Lista de Tarefas</C.Header>
+
+          {
+            /*Area de adicionar nova tarefa*/
+            <AddArea onEnter={handleAddTask} />
+          }
+
+          {
+            /*Area de exibir tarefas */
+            list.map((item,index) => (
+              <ListItem key={index} item={item} />
+            ))
+            
+          }
+
+          
+
+        </C.Area>
+    </C.Container>
   );
 }
 
---
+++ src/components/AddArea/index.tsx
@@ -0,0 +1,28 @@
+import { useState, KeyboardEvent } from 'react';
+import * as C from './style'
+
+type Props = {
+  onEnter: (taskName: string) => void
+}
+
+export const AddArea = ({onEnter}: Props) => {
+  const [inputText, setInputText] = useState('');
+
+  const handleKeyUp = (e: KeyboardEvent) => {
+    console.log(e.code);
+    if(e.code === 'Enter' && inputText !== '') {
+      onEnter(inputText);
+      setInputText('');
+    } else if (e.code === 'NumpadEnter' && inputText !== '') {
+      onEnter(inputText);
+      setInputText('');
+    }
+  }
+
+  return (
+    <C.Container>
+      <div className="image">+</div>
+      <input type="text" placeholder="Adicione uma tarefa" value={inputText} onChange={e=>setInputText(e.target.value)} onKeyUp={handleKeyUp}/>
+    </C.Container>
+  );
+}
